afcfd95ebfff296eee1a693bfc244bccd3bc2e48
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-01-26 10:52:13 -0600

    completed cycle 4

--- routes/tests.js
+++ routes/tests.js
@@ -1,4 +1,37 @@
 const router = require('express').Router();
+const Test = require('../db/models/test');
 
+router.get('/', async (req, res, next) => {
+  try {
+    const tests = await Test.findAll();
+    res.send(tests);
+  } catch (err) { next(err) };
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const test = await Test.findByPk(req.params.id);
+    res.send(test);
+  } catch (err) { next(err) };
+});
+
+router.post('/student/:id', async (req, res, next) => {
+  try {
+    const test = await Test.create(req.body);
+    await test.setStudent(Number(req.params.id));
+    res.status(201).send(test);
+  } catch (err) { next(err) };
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await Test.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.sendStatus(204);
+  } catch (err) { next(err) };
+});
 
 module.exports = router;
